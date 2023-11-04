52b59dbcef39b4a030f65766131a6bf6f64ad280
javiermh20 <javiermejiah20@gmail.com>
2023-11-04 14:01:18 -0600

    Actualizacion ya funciona y tiene estilos el nav y las rutas

--- sesion-04/ejercicios-talento-tech/src/App.jsx
+++ sesion-04/ejercicios-talento-tech/src/App.jsx
@@ -1,43 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Inicio from "./components/Inicio";
 import HelloWorld from "./components/HelloWorld";
 import Timer from "./components/Timer";
 
 function App() {
-  const [routes, setRoutes] = useState([
-    {
-      path: "/hello-world",
-      name: "Hello World",
-      component: HelloWorld,
-    },
-    {
-      path: "/timer",
-      name: "Timer",
-      component: Timer,
-    }
-  ]);
-
   return (
     <Router>
-      <div className="min-h[100vh] min-w-[100vw] bg-slate-400 relative"></div>
-      <nav className="absolute top-0 left-0 h-12">
-        <h1>Talento Tech</h1>
+      <nav className="bg-indigo-600 p-4">
+        <h1 className="text-white text-4xl font-semibold text-center">Talento Tech</h1>
       </nav>
-      <aside className="absolute left-0 top-8 bg-gray-900 text-white min-w-[150px]">
-        {routes.map((route) => (
-          <Link to={route.path}>
-            {route.path}
-            {route.name}
+      <div className="flex">
+        <aside className="bg-indigo-400 p-4 ">
+          <Link to="/hello-world" className="block text-white hover:underline my-2">
+            Hello World
           </Link>
-        ))}
-      </aside>
-      <main>
-        <Routes>
-          {routes.map((route) => (
-            <Route path={route.path} element={route.component} key={`route-${route.path}`} />
-          ))}  
-        </Routes>
-      </main>
+          <Link to="/timer" className="block text-white hover:underline my-2">
+            Timer
+          </Link>
+        </aside>
+        <main className="flex-1 p-4">
+          <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/hello-world" element={<HelloWorld />} />
+            <Route path="/timer" element={<Timer />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
